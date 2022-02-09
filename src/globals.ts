289992b51cb02f4cb92cf0881f4a8424e4f3ed39
pathakankit99 <ankitpathak143192@gmail.com>
2022-02-09 07:22:38 +0530

    initial commit

---
+++ src/globals.ts
@@ -0,0 +1,11 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const DEFAULTS = {
+  CLUSTER: 'mainnet',
+  GEM_BANK_PROG_ID: new PublicKey(
+    '3U31UreiQnwVz68wX8oQcLWETKkZL48JH3uW9yMMbY4y'
+  ),
+  GEM_FARM_PROG_ID: new PublicKey(
+    'VHhZHmwUKRm4b2tstD1o88UNXPLkUg7B3taAeeD67yQ'
+  ),
+};
